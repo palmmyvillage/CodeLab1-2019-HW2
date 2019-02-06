9da41511af574a78baf0ee978c62a82fa6a4f147
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-02-06 11:43:46 -0500

    Ini all HW2 file

---
+++ CodeLap week 02/Assets/Script/Homework/OnGroundCHeck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundCHeck : MonoBehaviour
+{
+    public int playerNumber;
+    public PlayerController Controller;
+    
+    private void OnCollisionStay2D(Collision2D other) //check collisionEnter to enable jump in PlayerController
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Controller.Player[playerNumber].jumpEnable = true;
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D other) //check collisionExit to disable jump in PlayerController
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Controller.Player[playerNumber].jumpEnable = false;
+        }
+    }
+}
